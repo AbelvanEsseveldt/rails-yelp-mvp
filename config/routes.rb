7deb3f3243364b813668a94092b00405b7573741
Abel van Esseveldt <abelvess@gmail.com>
2020-02-19 15:07:12 +0100

    correct routes, controllers, models

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'restaurants/index'
-  get 'restaurants/new'
-  get 'restaurants/show'
-  get 'restaurants/create'
+
+  resources :restaurants, only: [:index, :new, :show, :create]  do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,13 @@
 #
 # Examples:
 #
+
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurants = Restaurant.create!([
+ {name: "LeWagon", category: "chinese", address: "Ferdinand Bolstraat 5", phone_number: "020-35123520"},
+ {name: "MacDonalds", category: "japanese", address: "Hugo Grootstraat 2", phone_number: "020-15123520"},
+ {name: "Burger King", category: "chinese", address: "Kinkerstraat 1", phone_number: "020-25123520"},
+ {name: "KFC", category: "french", address: "Frankstraat 2", phone_number: "020-651232450"},
+ {name: "Dining In Dark", category: "belgian", address: "Rustenburgerstraat 1", phone_number: "020-45123520"}
+])
